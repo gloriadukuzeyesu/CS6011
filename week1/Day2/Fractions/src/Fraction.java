a993addba8908c8f91f58c76525b62b279440293
Gloria Dukuzeyesu <gloriadukuzeyesu@glorias-mbp.uconnect.utah.edu>
2022-09-27 19:34:29 -0600

    adding fractions project

---
+++ week1/Day2/Fractions/src/Fraction.java
@@ -0,0 +1,128 @@
+import org.jetbrains.annotations.NotNull;
+
+public class Fraction {
+    //declaration of variables
+    long numerator_;
+    long denominator_;
+
+    //default constructor
+    public Fraction(){
+        long numerator = 0;
+        long denominator = 1;
+    }
+
+    // A constructor which sets the value of the fraction to a specific numerator (n) and denominator (d).
+    public  Fraction ( long n, long d ) {
+        numerator_ = n;
+        denominator_ = d;
+        reduce(); // simply the numerator  and denominator right away
+         if ( numerator_ < 0 && denominator_ < 0) {
+             numerator_ = numerator_ * -1;
+             denominator_ = denominator_ * -1;
+         }else if ( denominator_ < 0 ){
+             numerator_ = numerator_ * -1 ;
+             denominator_ = denominator_ * -1;
+             }
+    }
+
+    // create the set and get for numerator and denominator
+    public long getNumerator (){
+        return numerator_;
+    }
+
+    public long getDenominator() {
+        return denominator_;
+    }
+
+    public void setNumerator (long numerator) {
+        numerator_ = numerator;
+    }
+
+    public void setDenominator ( long denominator) {
+        denominator_ = denominator;
+    }
+
+    // add fractions together
+    public Fraction plus( Fraction rhs ){
+        long tempNum = ( numerator_ * rhs.getDenominator() + denominator_ * rhs.getNumerator() );
+        long tempDen = denominator_ * rhs.getDenominator();
+        // instantiate a new fraction to store the sum
+        return new Fraction (tempNum, tempDen);
+    }
+
+    //subtract fractions together
+    public Fraction minus(Fraction rhs){
+        long tempNum = ( numerator_ * rhs.getDenominator() - denominator_ * rhs.getNumerator() );
+        long tempDen = denominator_ * rhs.getDenominator();
+        return new Fraction (tempNum, tempDen);
+    }
+
+    //multiply two functions
+    public Fraction times(Fraction rhs){
+        long numerator = numerator_ * rhs.getNumerator();
+        long denominator = denominator_ * rhs.getDenominator();
+        return new Fraction(numerator, denominator);
+    }
+
+    public  Fraction dividedBy(Fraction rhs){
+        long numerator = numerator_ * rhs.getDenominator();
+        long denominator = denominator_ * rhs.getNumerator();
+        return new Fraction(numerator, denominator);
+    }
+
+    public Fraction reciprocal(){
+        long newDenominator = numerator_;
+        long newNumerator = denominator_;
+        return new Fraction(newNumerator, newDenominator);
+    }
+    // toString() returns a string representation of the fraction
+    @Override
+    public String toString(){
+        return this.numerator_ + "/" + this.denominator_;
+    }
+    //Returns a (double precision) floating point number that
+    //is the approximate value of this fraction, printed as a real number.
+    //cast
+    public double toDouble(){
+        return (double) numerator_ / denominator_;
+    }
+
+   // repetitive division
+
+//    private  long GCD( long numerator, long denominator){
+//        if ( (numerator % denominator) == 0 ){
+//            return denominator;
+//        }
+//        else {
+//            // call the GCD() again as long as (numerator % denominator) != 0
+//            // replace the denominator with numerator % denominator
+//            return  GCD(denominator, numerator % denominator);
+//        }
+//    }
+
+
+
+    private long GCD (long numerator, long denominator) {
+        long gcd = numerator;
+        long remainder = denominator;
+        while( remainder != 0 ) {
+            long temp = remainder;
+            remainder = gcd % remainder;
+            gcd = temp;
+        }
+        return gcd;
+    }
+
+
+    private void reduce(){
+        // determine the greatest common divisor
+        long gcd = GCD(numerator_, denominator_);
+        // in case gcd is negative change it to positive
+        if (gcd < 0 ){
+            gcd = -gcd;
+        }
+        //divide both numerator and denominator with gcd
+        numerator_ = numerator_ / gcd;
+        denominator_ = denominator_ / gcd;
+    }
+}
---
+++ week1/Day2/Fractions/src/FractionTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FractionTest {
+
+    @Test
+    public void runAllTests() {
+        //Test for toString()
+        Fraction f1 = new Fraction( 3, -9);
+        String myString = f1.toString();
+        System.out.println( f1+ " changed to string is: " + myString);
+        Assertions.assertEquals(myString, "-1/3");
+
+        // Test for toDouble ()
+        Fraction f2 = new Fraction( 12, 8);
+        double myDouble = f2.toDouble();
+        Assertions.assertEquals(myDouble, 1.50);
+        System.out.println(f2 + " changed to double is: " + myDouble);
+
+
+        // Test for getNumerator() and getDenominator()
+        System.out.println("The denominator of " + f1 + " is " + f1.getDenominator());
+        System.out.println("The numerator of "+ f1 + " is " + f1.getNumerator());
+
+        // Test for Fraction plus
+        Fraction f3 = f1.plus(f2);
+        Assertions.assertEquals(7/6, 7/6);
+        System.out.println("The sum of " + f1 + " and " + f2 + " is " + f3 );
+
+        // Test for minus
+        Fraction f4 = f1.minus(f2);
+        Assertions.assertEquals(-11/6, -11/6);
+        System.out.println("The result from  subtracting" + f1 + " and " + f2 + " is " + f4);
+
+        // Test for times
+        Fraction f5;
+        f5 = f3.times(f1);
+        Assertions.assertEquals(-7/18, -7/18);
+        System.out.println("The result from  multiplying "+ f3 + " with " + f1 + " is " + f5);
+
+        //Test for dividedBy()
+        Fraction f6 = f2.dividedBy(f1);
+        Assertions.assertEquals(-9/2, -4);
+        System.out.println("The result from  division of "+ f2 + " and " + f1 + " is " + f6);
+
+
+        //Test for reciprocal()
+        Fraction f7 = f2.reciprocal();
+        Assertions.assertEquals(2/3, 2/3);
+        System.out.println("The reciprocal of " + f2+ " is " + f7);
+
+
+    }
+
+}
