887d827228b6390ea29355047b502d57592c901d
Gloria Dukuzeyesu <gloriadukuzeyesu@glorias-mbp.uconnect.utah.edu>
2022-11-03 11:26:39 -0600

    final copy for android

--- Week5/Day22/AndroidChatClient/app/src/main/java/com/example/androidchatclient/MainActivity.java
+++ Week5/Day22/AndroidChatClient/app/src/main/java/com/example/androidchatclient/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.androidchatclient;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -51,5 +53,6 @@ public class MainActivity extends AppCompatActivity {
 
         ws_.sendText("join "+Username + " " + room_name);
     }
+
 
 }
